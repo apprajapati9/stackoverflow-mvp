f9b75a6fa44131e5d529f589b6f2ba6d3620f62e
Ajay P. Prajapati <apprajapati@hotmail.com>
2024-09-25 15:14:07 -0400

    added factory pattern and abstract factory pattern with tests

---
+++ app/src/test/java/com/apprajapati/mvp_stackoverflow/design_patterns/factory_method/AbstractionFactory.kt
@@ -0,0 +1,127 @@
+package com.apprajapati.mvp_stackoverflow.design_patterns.factory_method
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+/*
+    Abstraction factory method provides a way to access functionality without
+    caring about implementation.
+    The Abstract Factory Method is a design pattern that provides an interface for
+    creating families of related or dependent objects without specifying their
+    concrete classes. This pattern is particularly useful in scenarios
+    where a system needs to be independent of how its objects are created,
+    composed, and represented.
+
+    It has one level of abstraction over the factory pattern shown in FactoryMethod.kt file.
+    provides further separation of concerns
+
+    The Abstract Factory Method is useful in scenarios where you want to:
+
+    - Create families of related objects without tightly coupling your code to specific classes.
+    - Manage multiple product families, allowing for easy swapping and changes in implementation.
+    - Keep your system flexible and adaptable to future changes in product specifications.
+ */
+
+interface DataSource {
+    fun printFactoryName(): String
+}
+
+class DatabaseDataSource : DataSource {
+    override fun printFactoryName(): String = "Database Factory"
+}
+
+class NetworkDataSource : DataSource {
+    override fun printFactoryName(): String = "Network Factory"
+}
+
+abstract class DataSourceFactory {
+
+    //concrete implementation needed for this method.
+    abstract fun createDataSource() : DataSource
+
+    companion object{
+        inline fun  <reified T: DataSource> createFactory() : DataSourceFactory =
+            when(T::class){
+                DatabaseDataSource::class -> {
+                    DBFactory()
+                }
+                NetworkDataSource::class -> {
+                    NetworkFactory()
+                }
+                else -> {
+                    throw IllegalArgumentException("No such class exists! Please provide correct one.")
+                }
+            }
+    }
+}
+
+//Concrete class of abstract factory i.e specific factory -> Network Factory
+class NetworkFactory : DataSourceFactory(){
+    override fun createDataSource(): DataSource  = NetworkDataSource()
+}
+
+//Concrete class of abstract factory i.e specific factory -> Database Factory
+class DBFactory : DataSourceFactory(){
+    override fun createDataSource(): DataSource = DatabaseDataSource()
+
+}
+
+class AbstractionFactoryTest {
+    @Test
+    fun testAbstractFactory(){
+        //abstract class don't need instantiation as well.
+        val networkDataFactory = DataSourceFactory.createFactory<NetworkDataSource>()
+        val dataSource = networkDataFactory.createDataSource()
+
+        println("created datasource $dataSource")
+        Assertions.assertThat(dataSource).isInstanceOf(NetworkDataSource::class.java)
+
+        println(dataSource.printFactoryName())
+        //prints factory name, shows how dataSource object is of interface yet prints method of given concrete class.
+    }
+}
+
+/*
+    Diagram
+     ----> DataSource Interface
+          -->DatabaseDataSource /NetworkDataSource  (both implements Datasource interface)
+     ----> DataSourceFactory - Abstract factory - thus needed concrete implementation
+        --> NetworkFactory / DBFactory - concrete implementations
+
+    So in a way in this example,
+        DatabaseFactory is an abstract class that has 2 methods
+            1. createDataSource - returns DataSource interface instance
+            2. createFactory method -  returns a concrete instance of Datasource i.e classes that implements Datasource
+                       DatabaseDataSource and DatabaseDataSource implements DataSource
+
+        Now concrete implementation of DatabaseFactory
+             NetworkFactory
+             DBFactory
+        So both classes will have to provide concrete implementation of createDataSource functionality and return a datasource.
+ */
+
+
+//Example attempt. - incomplete.
+//interface CurrencySymbol {
+//    fun currency(): String
+//}
+//
+//sealed class Countries
+//
+//class USAImpl : Countries(), CurrencySymbol {
+//    override fun currency(): String = "USA"
+//}
+//
+//class UK : Countries(), CurrencySymbol {
+//    override fun currency(): String = "POUND"
+//}
+//
+//abstract class CountryFactory {
+//    abstract fun createFactory() : CurrencySymbol
+//
+//    companion object {
+//        inline fun <reified T: CurrencySymbol> createCountryCurr() {
+//
+//        }
+//    }
+//}
---
+++ app/src/test/java/com/apprajapati/mvp_stackoverflow/design_patterns/factory_method/FactoryMethod.kt
@@ -0,0 +1,51 @@
+package com.apprajapati.mvp_stackoverflow.design_patterns.factory_method
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+/*
+    Factory method provides a way to access functionality without caring about implementation
+
+    This could provide us with
+        Separation of concerns
+        allows testability
+ */
+
+
+sealed class Country {
+    object Canada : Country()
+}
+
+object Spain : Country()
+class Greece(val place: String) : Country()
+data class USA(val capital : String) : Country()
+//class UAE : Country()
+
+class Currency (val code : String)
+
+object CurrencyFactory {
+    fun currencyForCountry(country: Country) : Currency {
+        return when(country){
+            is Spain -> Currency("EUR")
+            is Greece -> Currency("EUR")
+            is USA -> Currency("USD")
+            is Country.Canada -> Currency("CAD")
+        }
+    }
+}
+
+class FactoryMethodTest {
+
+    @Test
+    fun currencyTest(){
+        val greekCurrency = CurrencyFactory.currencyForCountry(Greece("GRE")).code
+        println("Greek currency $greekCurrency")
+
+        val usaCurr = CurrencyFactory.currencyForCountry(USA("GRE")).code
+        println("USA currency $usaCurr")
+
+        Assertions.assertThat(greekCurrency).isEqualTo("EUR")
+        Assertions.assertThat(usaCurr).isEqualTo("USD")
+
+    }
+}
